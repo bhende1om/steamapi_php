2c2af3987b66f8c79168b4ef0ed918c592dfec0d
Om Bhende <om@curtcreation.net>
2016-06-05 10:34:37 +0530

    Removed "info" from config array
    Removed $config['info'] since it was of no use.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 */
 $config = array(
   "api_key" => "Steam API Key",
-  "info" => "at0mic's official api server (v1.0.0)"
 );
 $steamid = $_GET['id']; // ?id=<player id>
 $url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" . $config['api_key'] . "&steamids=$steamid";
